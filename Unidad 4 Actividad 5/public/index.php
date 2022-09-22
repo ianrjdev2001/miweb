0a881b9382cf920599ef129aca6c145ee37250b6
ianrjdev2001 <109057188+ianrjdev2001@users.noreply.github.com>
2022-09-22 02:14:04 -0600

    Add files via upload

---
+++ Unidad 4 Actividad 5/public/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso al panel</title>
+    <!--script src="https://cdn.tailwindcss.com"></script-->
+    <!--link rel="stylesheet" href="public/css/main.css"-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row d-flex justify-content-center mt-4">
+            <div class="col-4 card">
+                <form>
+                    <fieldset>
+                        <div class="text-center mt-4">
+                            <h1>Login</h1>
+                            <legend >Datos de acceso</legend>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <label class="form-label">Email:</label>
+                                <input type="text" name="" placeholder="write here" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <label class="form-label">Password:</label>
+                                <input type="password" name="" placeholder="* * * * *" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mt-4 mb-4">
+                            <div class="col-12 d-flex justify-content-center">
+                                <button type="submit">Acceder</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+            
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
